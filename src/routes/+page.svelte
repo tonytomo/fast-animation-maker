203d403edef57e70dfa1d8d1da417495e9c92c55
tonytomo <fathonisatrioutomo@gmail.com>
2025-11-06 15:11:56 +0700

    feat: create the base

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,12 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	let code = $state('');
+</script>
+
+<main class="flex min-h-screen flex-col">
+	<div class="flex flex-1 *:w-full">
+		<div class="flex-1 bg-neutral-800"></div>
+		<div class="relative flex flex-2 items-center justify-center bg-neutral-950 px-4">
+			<div class="aspect-video w-full max-w-4xl bg-white"></div>
+		</div>
+	</div>
+</main>
